7344fb1e074dbd1568c31a24ffc9dc3020f19a1a
Angga-ops <54893918+Angga-ops@users.noreply.github.com>
2020-11-10 21:02:44 +0700

    menambahkan file

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+  //include "rss.php";
+  
+  //$w=mysql_fetch_array(mysql_query("select alamat_website from identitas"));
+  
+  //header("location:$w[alamat_website]");
+  header("location:redaktur");
+?>
